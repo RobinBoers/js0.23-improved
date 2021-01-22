d6507974777616bdd727bbc0a7127762c6064f1b
Robin Boers <60298132+RobinBoers@users.noreply.github.com>
2021-01-22 09:59:34 +0100

    Made functional mod switch

---
+++ assets/js/loader.js
@@ -0,0 +1,14 @@
+var script = document.createElement("script");  // create a script DOM node
+const localStorage = window.localStorage;
+
+if(localStorage.getItem("modded") === null) {
+    localStorage.setItem("modded", "false");
+} 
+
+if(localStorage.getItem("modded") === "true") {
+    script.src = "assets/js/modded.js?4e4e005077c1d4940468"; 
+} else {
+    script.src = "assets/js/app198e.js?4e4e005077c1d4940468"; 
+}
+
+document.head.appendChild(script);
